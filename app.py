39946049d7f16f096a4811c761911facb19c8537
Saket Saumya <34890341+saket13@users.noreply.github.com>
2021-03-28 19:14:54 +0530

    Added API endpoint /events

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ default_app = initialize_app(cred)
 db = firestore.client()
 
 
-# To get raffle tickets 
+# 1. To get raffle tickets 
 # Database only storing counts and updating count value in scalable manner (Atomically)
 # Returning the count value by making GET request for issuing the ticket ID to the currently logged in user
 @app.route('/get_ticket', methods=['GET'])
@@ -34,6 +34,23 @@ def get_raffle_tickets():
 
 
 
+# 2. To post a new event or get events 
+# POST : Post a new event in the raw JSON format {"date" : "25032021", "item" : "Fridge"}
+# GET  : Get details of all the upcoming events
+@app.route('/events', methods=['GET', 'POST'])
+def get_or_post_event_date():
+    try:
+        events_ref = db.collection(u'events')
+        if request.method == 'POST':
+            event_date = request.json['date']
+            events_ref.document(event_date).set(request.json)
+            return jsonify({"Event added successfully": True}), 200
+        
+        else:
+            all_events = [event.to_dict() for event in events_ref.stream()]
+            return jsonify(all_events), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 
 
